aa0d2708c2948caa24bcf2ee38ba0dd4d77f2f4b
Lanking <lanking520@live.com>
2017-11-28 19:25:55 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@ echo "Checking for CUDA and installing."
 # Check for CUDA and try to install.
 # The 16.04 installer works with 16.10.
 curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-apt-get update
-apt-get install cuda-8-0 -y
+sudo dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo apt-get update
+sudo apt-get install cuda-8-0 -y
 # install cuDNN v6.0
 CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
 wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
