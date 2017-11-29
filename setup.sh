8553d462a06ef466459a43f6650c568208b3cfbb
Lanking <lanking520@live.com>
2017-11-28 19:17:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 echo "Checking for CUDA and installing."
 # Check for CUDA and try to install.
-if ! dpkg-query -W cuda-8-0; then
-  # The 16.04 installer works with 16.10.
-  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-  dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-  apt-get update
-  apt-get install cuda-8-0 -y
-fi
+# The 16.04 installer works with 16.10.
+curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+dpkg -i ./cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+apt-get update
+apt-get install cuda-8-0 -y
 # install cuDNN v6.0
 CUDNN_TAR_FILE="cudnn-8.0-linux-x64-v6.0.tgz"
 wget http://developer.download.nvidia.com/compute/redist/cudnn/v6.0/${CUDNN_TAR_FILE}
@@ -20,8 +18,8 @@ sudo chmod a+r /usr/local/cuda-8.0/lib64/libcudnn*
 export PATH=/usr/local/cuda-8.0/bin${PATH:+:${PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda-8.0/lib64/${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 
-sudo apt-get install libcupti-dev
-sudo apt-get install python3-pip python3-dev
+sudo apt-get -y install libcupti-dev
+sudo apt-get -y install python3-pip python3-dev
 pip3 install --upgrade tensorflow-gpu
 sudo apt-get -y install ipython ipython-notebook
 sudo -H pip3 install jupyter
